e4cea7bc4c912c91c43a456ca1a624c7a54b8652
Andreas <152264387+Andrea21xd@users.noreply.github.com>
2026-03-20 15:26:01 +0100

    Initial project

---
+++ AIRacingGame/Source/AIRacingGame/AIRacingGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AIRacingGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AIRacingGame, "AIRacingGame" );
